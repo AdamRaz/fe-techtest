ebc9785bef7abf2c3f243d335e52dc1fb0b80033
Adam Razzak <a.abdel@hotmail.co.uk>
2018-12-03 13:41:17 +0000

    feat: initial commit - number card creation

--- app/app.js
+++ app/app.js
@@ -3,6 +3,16 @@ var app = {
   // this is the entry point for your app.
   init: function() {
     console.log("hello from app!");
+
+    // AR - create a list of 144 elements and append to number container section
+
+    let numberCardElement = ``;
+    let allNumberCardElements = ``;
+    for (let i = 1; i < 145; i++) {
+      numberCardElement = `<div><p class="number-card">${i}</p></div>`
+      allNumberCardElements += numberCardElement;
+    }
+    document.querySelector('.number-container').innerHTML = allNumberCardElements;
   }
 };
 
