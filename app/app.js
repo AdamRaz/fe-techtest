3e7caf71895e9fb9ca103c4dac98071b2b62a0ba
Adam Razzak <a.abdel@hotmail.co.uk>
2018-12-03 15:09:44 +0000

    feat: highlight number multiples with toggled class

--- app/app.js
+++ app/app.js
@@ -5,14 +5,43 @@ var app = {
     console.log("hello from app!");
 
     // AR - create a list of 144 elements and append to number container section
-
+    // AR - efficient way of adding elements? Advice to not use innerHTML?
+    // AR - no ES6? no template literals
     let numberCardElement = ``;
     let allNumberCardElements = ``;
     for (let i = 1; i < 145; i++) {
       numberCardElement = `<div><p class="number-card">${i}</p></div>`
       allNumberCardElements += numberCardElement;
     }
-    document.querySelector('.number-container').innerHTML = allNumberCardElements;
+    let numberContainer = document.querySelector('.number-container');
+    numberContainer.innerHTML = allNumberCardElements;
+    
+    let allCards = document.querySelectorAll('.number-card');
+
+    toggleMultiples = function (e) {
+      let target = e.target;
+      if (target.classList.contains('number-card')){
+        console.log("number card!");
+        console.log(target.innerText);
+        let clickedNumber = Number.parseInt(target.innerText);
+        // console.log(allCards);
+        allCards.forEach(function (element) {
+          let otherCardNumber = element.innerText;
+          if  (otherCardNumber % clickedNumber === 0) {
+            console.log(`${otherCardNumber} is a multiple`)
+            element.classList.toggle('highlight-card');
+          }
+        });
+
+        
+
+      }
+      // console.log("click!");
+      // console.log(e.target);
+      // let divContainer = e.
+    
+    }
+    numberContainer.addEventListener('click', toggleMultiples);
   }
 };
 
